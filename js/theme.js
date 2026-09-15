4fdd76b3968d3958200be42e7b6e88f476772a97
2594959341-sketch <2594959341@qq.com>
2026-09-15 13:25:51 +0800

    deploy v293: photo import diagnostics + robustness (fix silent drop)

--- js/theme.js
+++ js/theme.js
@@ -7,8 +7,8 @@
  */
 (function () {
   var THEMES = {
-    cat:   { dir: 'png',   ver: 'mumu-v292' },
-    zhiya: { dir: 'zhiya', ver: 'mumu-v292' }
+    cat:   { dir: 'png',   ver: 'mumu-v293' },
+    zhiya: { dir: 'zhiya', ver: 'mumu-v293' }
   };
   var KEY = 'mumu_theme';
   var raw = localStorage.getItem(KEY);
