d776ae35a1e0a95a4bce2fb2e705b9c4b73ca235
2594959341-sketch <2594959341@qq.com>
2026-09-15 15:51:17 +0800

    v298: 修复 SW 缓存陈旧导致新代码拉不下来（注册改纯 sw.js + 清理旧注册 + 设置页版本号/检查更新）

--- js/theme.js
+++ js/theme.js
@@ -7,8 +7,8 @@
  */
 (function () {
   var THEMES = {
-    cat:   { dir: 'png',   ver: 'mumu-v297' },
-    zhiya: { dir: 'zhiya', ver: 'mumu-v297' }
+    cat:   { dir: 'png',   ver: 'mumu-v298' },
+    zhiya: { dir: 'zhiya', ver: 'mumu-v298' }
   };
   var KEY = 'mumu_theme';
   var raw = localStorage.getItem(KEY);
