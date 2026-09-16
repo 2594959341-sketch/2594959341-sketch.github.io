b1ae6cea3248e3822d391dd75abd2440dfb10fa2
2594959341-sketch <2594959341@qq.com>
2026-09-16 22:36:50 +0800

    v315: 1)fix 备考页月经假色块被错误染成绿色(日历 restSet 不再并入月经假,月经假单独走 menstrualSet 上粉色,火花/连续天数口径不变) 2)常识错题题型移除冗余的"常识判断"(科目大类),只留真正子类型:科技常识/政治/法律/经济/人文历史/地理国情/管理公文,刷错题可直接选科技常识等

--- js/theme.js
+++ js/theme.js
@@ -7,8 +7,8 @@
  */
 (function () {
   var THEMES = {
-    cat:   { dir: 'png',   ver: 'mumu-v314' },
-    zhiya: { dir: 'zhiya', ver: 'mumu-v314' }
+    cat:   { dir: 'png',   ver: 'mumu-v315' },
+    zhiya: { dir: 'zhiya', ver: 'mumu-v315' }
   };
   var KEY = 'mumu_theme';
   var raw = localStorage.getItem(KEY);
