e70ce637a2c3c6f9689cb91d1bd56163c2d4ee8c
2594959341-sketch <2594959341@qq.com>
2026-09-24 09:40:16 +0800

    deploy mumu-v338 : js/theme.js

--- js/theme.js
+++ js/theme.js
@@ -7,8 +7,8 @@
  */
 (function () {
   var THEMES = {
-    cat:   { dir: 'png',   ver: 'mumu-v337' },
-    zhiya: { dir: 'zhiya', ver: 'mumu-v337' }
+    cat:   { dir: 'png',   ver: 'mumu-v338' },
+    zhiya: { dir: 'zhiya', ver: 'mumu-v338' }
   };
   var KEY = 'mumu_theme';
   var raw = localStorage.getItem(KEY);
